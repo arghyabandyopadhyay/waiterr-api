c872589577e74248f6abd6c3593a844901cae2c2
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-09-24 16:52:05 +0530

    Create waiterrMenuGroup.js

---
+++ services/waiterrMenuGroup.js
@@ -0,0 +1,76 @@
+const db = require("./db");
+const helper = require("../helper");
+
+async function get(guid) {
+  const result = await db.query(
+    `SELECT * FROM MenuGroup WHERE ClientId='${guid}'`
+  );
+  const data = helper.emptyOrRows(result);
+
+  if(data.length>0)return data;
+  else return ;
+}
+
+async function create(runningOrder) {
+  const result = await db.query(
+    `INSERT INTO RunningOrder 
+    (Name,MobileNo,SalePointType,SalePointName,WaiterName,Amount,PAX,BillPrinted,OutletName)
+    VALUES 
+    ('${runningOrder.Name}' ,'${runningOrder.MobileNo}' ,'${runningOrder.SalePointType}' ,'${runningOrder.SalePointName}','${runningOrder.WaiterName}','${runningOrder.Amount}',${runningOrder.PAX},${runningOrder.BillPrinted},'${runningOrder.OutletName}');`
+  );
+
+  let message = "Error in creating running order";
+
+  if (result.affectedRows) {
+    message = "Running order created successfully";
+  }
+
+  return { message };
+}
+
+async function update(id, runningOrder) {
+  const result = await db.query(
+    `UPDATE RunningOrder 
+    SET 
+    Name= '${runningOrder.Name}' ,
+    MobileNo= '${runningOrder.MobileNo}' ,
+    SalePointType= '${runningOrder.SalePointType}' ,
+    SalePointName= '${runningOrder.SalePointName}',
+    WaiterName= '${runningOrder.WaiterName}',
+    Amount= '${runningOrder.Amount}',
+    PAX= ${runningOrder.PAX},
+    ActiveSince= '${runningOrder.ActiveSince}',
+    BillPrinted= ${runningOrder.BillPrinted},
+    OutletName= '${runningOrder.OutletName}'
+    WHERE id = '${id}'`
+  );
+
+  let message = "Error in updating user Details";
+
+  if (result.affectedRows) {
+    message = "User Details updated successfully";
+  }
+
+  return { message };
+}
+
+async function remove(id) {
+  const result = await db.query(
+    `DELETE FROM RunningOrder WHERE id='${id}'`
+  );
+
+  let message = "Error in deleting running order";
+
+  if (result.affectedRows) {
+    message = "Running order deleted successfully";
+  }
+
+  return { message };
+}
+
+module.exports = {
+  get,
+  create,
+  update,
+  remove,
+};
