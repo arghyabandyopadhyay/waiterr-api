a9707090ef60e4d779650323d7041b164464a6ef
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-10-24 18:50:16 +0530

    Update salePointHistory.js

--- services/salePointHistory.js
+++ services/salePointHistory.js
@@ -10,6 +10,64 @@ async function get(salePointType, salePointName, outletId) {
   else return {statusCode:200, body:[]};
 }
 
+async function getForApproval(approvalType,guid) {
+  var condition="";
+  if(approvalType=="OrderApproved")condition="OrderApproved=0";
+  else if(approvalType=="OrderPrepared")condition="OrderApproved=1 AND OrderPrepared=0";
+  else if(approvalType=="OrderProcessed")condition="OrderApproved=1 AND OrderPrepared=1 AND OrderProcessed=0";
+  const result = await db.query(
+    `SELECT Orders.id AS OrderId, KotNumber, RunningOrderId, OutletName, SalePointType, SalePointName, Item, Quantity, Rate, isDiscountable AS Discountable, Discount AS DiscountPercent, TaxRate, OrderPlaced, OrderApproved, OrderPrepared, OrderProcessed From Orders LEFT JOIN RunningOrder ON Orders.RunningOrderId=RunningOrder.id LEFT JOIN Outlets ON RunningOrder.OutletId=Outlets.id LEFT JOIN MenuItem ON Orders.ItemId=MenuItem.id WHERE Orders.ClientId=? AND ${condition} ORDER BY ActiveSince,KOTNumber`,[guid]
+  );
+  const data = helper.emptyOrRows(result);
+  if(data.length>0)return {statusCode:200,body:data};
+  else return {statusCode:200, body:[]};
+}
+async function approveOrders(forApproval,forAggregate,runningOrderId,guid,kotNumber,approvalType,allProcessed){
+  if(forApproval){
+    const result = await db.query(
+      `UPDATE Orders 
+      SET 
+      ${approvalType}=?
+      WHERE ${forAggregate?"ClientId=?":"runningOrderId=? AND KotNumber=?"}`,forAggregate?[1,guid]:[1,runningOrderId,kotNumber]
+    );
+    let message = "Error in approving orders";
+    let statusCode=500;
+    if (result.affectedRows) {
+      message = "Success";
+      statusCode=200;
+      if(forAggregate){
+        if(approvalType=="OrderProcessed"){
+          //Generate bill now and send it as email to the individual user
+          const result1=await db.query(
+            `UPDATE RunningOrder
+            SET 
+            BillPrinted=1
+            WHERE ClientId=?`,[guid]
+          )
+        }
+      }
+      else{
+        if(approvalType=="OrderProcessed"&&allProcessed){
+          //Generate bill now and send it as email to the user
+          const result1=await db.query(
+            `UPDATE RunningOrder
+            SET 
+            BillPrinted=1
+            WHERE id=?`,[runningOrderId]
+          )
+        }
+      }
+    }
+    return { statusCode:statusCode, message:message };
+  }
+  else{
+
+  }
+  
+}
+
 module.exports = {
-  get
+  get,
+  getForApproval,
+  approveOrders
 };
