02576698380a62fc712e51db55a943be426aaefd
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-11-08 22:13:56 +0530

    Update userClientAllocation.js

--- services/userClientAllocation.js
+++ services/userClientAllocation.js
@@ -3,7 +3,7 @@ const helper = require("../helper");
 
 async function get(userId) {
   result = await db.query(
-    `SELECT Outlets.id, GUID, CompanyGUID, OutletName, OutletSalePoint, ClientName, LogoUrl, DataExchangeVia, DataExchangeUrl, UCARoleId FROM UserClientAllocation LEFT JOIN Outlets ON UserClientAllocation.OutletId=Outlets.id LEFT JOIN Clients ON Outlets.GUID = Clients.id WHERE UserId=?`,[userId]
+    `SELECT Outlets.id, GUID, CompanyGUID, OutletName, OutletSalePoint, ClientName, LogoUrl, DataExchangeVia, DataExchangeUrl, UCARoleId FROM UserClientAllocation INNER JOIN Outlets ON UserClientAllocation.OutletId=Outlets.id INNER JOIN Clients ON Outlets.GUID = Clients.id WHERE UserId=?`,[userId]
   );
   const data = helper.emptyOrRows(result);
   if(data.length>0){
@@ -33,7 +33,7 @@ async function get(userId) {
 
 async function getAllWaiters(guid) {
   result = await db.query(
-    `SELECT UserDetails.id AS id, Name, MobileNumber, RoleId, IsActive, last_login, Outlets.id AS OutletId, OutletName, UCARoleId FROM Clients LEFT JOIN Outlets ON Clients.id=Outlets.GUID LEFT JOIN UserClientAllocation ON Outlets.id=UserClientAllocation.OutletId LEFT JOIN UserDetails ON UserClientAllocation.UserId=UserDetails.id WHERE Clients.id=? ORDER BY UserClientAllocation.OutletId`,[guid]
+    `SELECT UserDetails.id AS id, Name, MobileNumber, RoleId, IsActive, last_login, Outlets.id AS OutletId, OutletName, UCARoleId FROM Clients INNER JOIN Outlets ON Clients.id=Outlets.GUID INNER JOIN UserClientAllocation ON Outlets.id=UserClientAllocation.OutletId INNER JOIN UserDetails ON UserClientAllocation.UserId=UserDetails.id WHERE Clients.id=? ORDER BY UserClientAllocation.OutletId`,[guid]
   );
   const data = helper.emptyOrRows(result);
   if(data.length>0){
